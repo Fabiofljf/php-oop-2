7d2a481b9ca1a8d111cd2847e6cd632f999f1c84
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-26 17:52:00 +0200

    Adding html

--- pages-web/home.php
+++ pages-web/home.php
@@ -1,21 +1,28 @@
-<?php 
+<?php
 include __DIR__ . '/../layout/header.php'
 
 ?>
 
 <main id="site_main">
 
-<h1>Benvenuto!</h1>
-<h2>
-    Clicca
-    <a href="/PHP/php-oop-2/pages-web/shop.php">qui</a>
-    per andare allo shop!
-</h2>
+    
+    <h1>Benvenuto!</h1>
+
+    <h2>
+        Clicca
+        <a href="/PHP/php-oop-2/pages-web/shop.php">qui</a>
+        per andare allo shop!
+    </h2>
+
+    <div class="img_central">
+        <img src="https://media.baamboozle.com/uploads/images/504628/1635844505_435597.jpeg" alt="">
+    </div>
+    <!-- /.jumbotron -->
 
 </main>
 <!-- /#site_main -->
 
-<?php 
+<?php
 include __DIR__ . '/../layout/footer.php'
 
 ?>
--- pages-web/shop.php
+++ pages-web/shop.php
@@ -1,14 +1,25 @@
-<?php 
+<?php
 include __DIR__ . '/../layout/header.php'
 
 ?>
 
 <main id="site_main">
-
+    <h2>Cibo per Animali</h2>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($foods as $food) : ?>
+            <div class="col">
+                <div class="card">
+                    <p><?= $fod->price; ?></p>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </main>
 <!-- /#site_main -->
 
-<?php 
+<?php
 include __DIR__ . '/../layout/footer.php'
 
 ?>
